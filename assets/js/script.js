7a8ca9e5729fefa7bfcf14b17326ae385b2272fa
NotSnowWhite <gflatch@att.net>
2024-04-05 18:15:54 -0500

    created function to create unique id and log in console

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,33 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
-// Todo: create a function to generate a unique task id
-function generateTaskId() {
+// modal script for buttons and modal
+const modal = document.getElementsByClassName('.modal');
+const exitButton = document.getElementsByClassName('close');
+const modalButton = document.getElementsByClassName('btn');
 
+
+// makes exit button remove modal
+exitButton.onclick = function () {
+    modal.style.display = 'none';
 }
 
+// function to call form on submit 
+const form = document.querySelector('Form');
+form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const taskTitle = document.getElementById('taskTitle').val();
+  console.log(taskTitle);
+  modal.style.display = 'none';
+});
+
+// Todo: create a function to generate a unique task id
+function generateTaskId() {
+const randomNum = Math.floor(Math.random() * 1001);
+return randomNum;
+}
+
+console.log(generateTaskId())
 // Todo: create a function to create a task card
 function createTaskCard(task) {
 
